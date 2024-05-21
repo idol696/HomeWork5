6f314a669e2ce36dcb935b171cf54c98dec748be
Roman Kuzmin <ghelatin@mail.ru>
2024-05-21 21:36:41 +0300

    init commit

--- src/Main.java
+++ src/Main.java
@@ -15,43 +15,40 @@ public class Main {
 
         System.out.println("Задача 2");
 
-        int clientDeviceYear = 2015;
+        clientOS = 1;
+        int yearMin = 2000;
+        int yearMax = 2024;
 
-        if (clientDeviceYear < 2015 && clientOS == 0) {
-            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-        } else if (clientDeviceYear < 2015 && clientOS == 1) {
-            System.out.println("Установите облегченную версию приложения для Android по ссылке");
-        } else if (clientDeviceYear >= 2015 && clientOS == 0) {
-            System.out.println("Установите версию приложения для iOS по ссылке");
-        } else if (clientDeviceYear >= 2015 && clientOS == 1) {
-            System.out.println("Установите версию приложения для Android по ссылке");
-        } else {
-            System.out.println("Ваша Система временно не поддерживается");
+        int clientDeviceYear = (int) (Math.random() * (yearMax - yearMin)) + yearMin;
+        System.out.println("Год выпуска: " + clientDeviceYear);
+
+        if (clientOS == 0) {
+
+            if (clientDeviceYear < 2015) {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            } else {
+                System.out.println("Установите версию приложения для iOS по ссылке");
+            }
+
+        } else if (clientOS == 1) {
+            if (clientDeviceYear < 2015) {
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            } else {
+                System.out.println("Установите версию приложения для Android по ссылке");
+            }
         }
+
 
         System.out.println("Задача 3");
 
         int year = 2000;
-        boolean yearLeapCheck = false;
 
-
-        if (year > 1584) {
-            if (year % 400 == 0) {
-                yearLeapCheck = true;
-            } else if (year % 100 == 0) {
-                yearLeapCheck = false;
-            } else if (year % 4 == 0) {
-                yearLeapCheck = true;
-            } else {
-                yearLeapCheck = false;
-            }
-        }
-
-        if (yearLeapCheck) {
+        if (year > 1584 && ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)) {
             System.out.println(year + " является високосным");
         } else {
             System.out.println(year + " не является високосным");
         }
+
 
         System.out.println("Задача 4");
 
@@ -59,19 +56,19 @@ public class Main {
         int deliveryDay = 0;
         int deliveryDistance = 95;
 
-        if ( deliveryDistance <20 ) {
-            deliveryDay = 1;
-        } else if ( deliveryDistance >= 20 && deliveryDistance <60 ) {
-            deliveryDay = 2;
-        } else if (deliveryDistance >= 60 && deliveryDistance <= 100 ) {
-            deliveryDay = 3;
-        }
-
-        if ( deliveryDay > 0 ) {
+        if (deliveryDistance <= 100) {
+            if (deliveryDistance < 20) {
+                deliveryDay = 1;
+            } else if (deliveryDistance >= 20 && deliveryDistance < 60) {
+                deliveryDay = 2;
+            } else if (deliveryDistance >= 60) {
+                deliveryDay = 3;
+            }
             System.out.println("Потребуется дней: " + deliveryDay);
         } else {
             System.out.println("Свыше 100 км доставки нет");
         }
+
 
         System.out.println("Задача 5");
 
